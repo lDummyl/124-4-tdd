379d9b9108a683cd0efdfe04dcbf796c2ecd33d1
Sergey Zhak <mail4shaidar@gmail.com>
2021-07-15 21:23:33 +0300

    Interpolator initial functional tests

--- src/test/java/users/homework/codetotest/LagrangeInterpolatorTest.java
+++ src/test/java/users/homework/codetotest/LagrangeInterpolatorTest.java
@@ -2,11 +2,28 @@ package users.homework.codetotest;
 
 import org.junit.Test;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import static org.junit.Assert.*;
 
 public class LagrangeInterpolatorTest {
 
     @Test
     public void getY_quadratic_function_1() {
+        LagrangeInterpolator li = getLagrangeInterpolator();
+        float x = 3;
+        assertEquals(3f, li.getY(x), 0.001f);
+    }
+
+    private LagrangeInterpolator getLagrangeInterpolator() {
+        LagrangeInterpolator lagrangeInterpolator = new LagrangeInterpolator();
+        List<Point> points = Stream.of(new Point(0f, 0f),
+                new Point(1f, 1f),
+                new Point(2f, 2f))
+                .collect(Collectors.toList());
+        lagrangeInterpolator.setPoints(points);
+        return lagrangeInterpolator;
     }
 }
--- src/test/java/users/homework/codetotest/NewtonInterpolatorTest.java
+++ src/test/java/users/homework/codetotest/NewtonInterpolatorTest.java
@@ -3,9 +3,26 @@ package users.homework.codetotest;
 import junit.framework.TestCase;
 import org.junit.Test;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class NewtonInterpolatorTest extends TestCase {
 
     @Test
     public void testGetY() {
+        NewtonInterpolator ni = getNewInterpolator();
+        float x = 3;
+        assertEquals(3f, ni.getY(x), 0.001f);
+    }
+
+    private NewtonInterpolator getNewInterpolator() {
+        NewtonInterpolator newtonInterpolator = new NewtonInterpolator();
+        List<Point> points = Stream.of(new Point(0f, 0f),
+                new Point(1f, 1f),
+                new Point(2f, 2f))
+                .collect(Collectors.toList());
+        newtonInterpolator.setPoints(points);
+        return newtonInterpolator;
     }
 }
