ff3094c5865b20ee8186078f1eab812e5b120b1c
Dima Ludov <lDummyl>
2021-07-15 20:26:00 +0300

    init

---
+++ src/test/java/users/homework/codetotest/NewtonInterpolatorTest.java
@@ -0,0 +1,11 @@
+package users.homework.codetotest;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class NewtonInterpolatorTest extends TestCase {
+
+    @Test
+    public void testGetY() {
+        }
+}
