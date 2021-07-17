557067acbf62a0fd3096974fb0faf04c5330aa58
Egor Vasilyev <em.vasiliev@outlook.com>
2021-07-17 14:23:55 +0300

    final changes

--- src/test/java/users/homework/codetotest/LagrangeInterpolatorTest.java
+++ src/test/java/users/homework/codetotest/LagrangeInterpolatorTest.java
@@ -1,52 +1,30 @@
 package users.homework.codetotest;
 
-import org.junit.Test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class LagrangeInterpolatorTest
 {
+    Interpolator interpolator;
+    List<Point> list;
 
-    @Test
-    public void getY_quadratic_function_1()
+    @BeforeEach
+    public void setup()
     {
-        Interpolator interpolator = new LagrangeInterpolator();
-        List<Point> list = new ArrayList<>();
-        for(float i = 0f; i <= 10f; i += 0.5f)
-        {
-            list.add(new Point(i, 2.3f * i * i + 4.3f * i + 8.2f));
-        }
-        interpolator.setPoints(list);
-        for(float i = 0.25f; i <= 10f; i += 0.5f)
-        {
-            assertEquals(2.3f * i * i + 4.3f * i + 8.2f,
-                    interpolator.getY(i),
-                    0.05f);
-        }
+        interpolator = new LagrangeInterpolator();
+        list = new ArrayList<>();
     }
-    @Test
-    public void getY_quadratic_function_2()
-    {
-        Interpolator interpolator = new LagrangeInterpolator();
-        List<Point> list = new ArrayList<>();
-        for(float i = 0f; i <= 10f; i += 0.5f)
-        {
-            list.add(new Point(i, i * i + 4.25f * i + 8.28f));
-        }
-        interpolator.setPoints(list);
-        for(float i = 0.25f; i <= 10f; i += 0.5f)
-        {
-            assertEquals(i * i + 4.25f * i + 8.28f, interpolator.getY(i), 0.1f);
-        }
-    }
+
     @Test
     public void getY_cubic_function()
     {
-        Interpolator interpolator = new LagrangeInterpolator();
-        List<Point> list = new ArrayList<>();
         for(float i = 0f; i <= 10f; i += 0.5f)
         {
             list.add(new Point(i, i * i * i + 3.2f * i * i + 7.72f * i + 4.32f));
@@ -59,30 +37,10 @@ public class LagrangeInterpolatorTest
                             0.05f);
         }
     }
-    @Test
-    public void getY_sine_function()
-    {
-        Interpolator interpolator = new LagrangeInterpolator();
-        List<Point> list = new ArrayList<>();
-        for(double i = 0; i <= 360; i += 30)
-        {
-            list.add(new Point((float) Math.toRadians(i),
-                    (float)Math.sin(Math.toRadians(i))));
-        }
-        interpolator.setPoints(list);
-        for(double i = 15; i <= 360; i += 30)
-        {
-            assertEquals((float) Math.sin(Math.toRadians(i)),
-                    interpolator.getY((float) Math.toRadians(i)),
-                    0.00001f);
-        }
-    }
 
     @Test
     public void getY_sine_quadratic_function()
     {
-        Interpolator interpolator = new LagrangeInterpolator();
-        List<Point> list = new ArrayList<>();
         for(double i = 0; i <= 360; i += 30)
         {
             list.add(new Point((float) i, quadric_sine_func(i)));
@@ -99,24 +57,42 @@ public class LagrangeInterpolatorTest
     @Test
     public void getY_sine_4th_order_function()
     {
-        Interpolator interpolator = new LagrangeInterpolator();
-        List<Point> list = new ArrayList<>();
         for(double i = 0; i <= 180; i += 15)
         {
-            list.add(new Point((float) i, break_it_for_sure(i)));
+            list.add(new Point((float) i, func_4th_order(i)));
         }
         interpolator.setPoints(list);
         for(double i = 5; i <= 180; i += 15)
         {
-            assertEquals(break_it_for_sure(i),
+            assertEquals(func_4th_order(i),
                         interpolator.getY((float) i),
                         0.01f);
         }
     }
 
-    private float break_it_for_sure(double x)
+    @Test
+    public void getY_tan()
     {
-        return (float) (8.431f * Math.pow(Math.cos(Math.toRadians(x)), 4) + 3.483f * Math.pow(Math.sin(Math.toRadians(x)), 3) + 9.388f * Math.pow(Math.sin(Math.toRadians(x)), 2)
+        for(double i = 0; i <= 180; i += 3)
+        {
+            list.add(new Point((float) i, tan_func(i)));
+        }
+        interpolator.setPoints(list);
+        for(double i = 1.5; i <= 180; i += 3)
+        {
+            assertEquals(tan_func(i),
+                    interpolator.getY((float) i),
+                    0.1f);
+        }
+    }
+
+
+
+    private float func_4th_order(double x)
+    {
+        return (float) (8.431f * Math.pow(Math.cos(Math.toRadians(x)), 4)
+                + 3.483f * Math.pow(Math.sin(Math.toRadians(x)), 3)
+                + 9.388f * Math.pow(Math.sin(Math.toRadians(x)), 2)
                 + 1.333f * Math.cos(Math.toRadians(x)) + 23.1222f);
     }
 
@@ -124,4 +100,10 @@ public class LagrangeInterpolatorTest
     {
         return (float) (Math.pow(Math.sin(Math.toRadians(x)), 2) + 7.921f * Math.sin(Math.toRadians(x)) + 8.534f);
     }
+
+    private float tan_func(double x)
+    {
+        return (float) (Math.pow(Math.tan(Math.toRadians(x * 0.5)), 2) - Math.tan(Math.toRadians(x)));
+    }
+
 }
--- src/test/java/users/homework/codetotest/NewtonInterpolatorTest.java
+++ src/test/java/users/homework/codetotest/NewtonInterpolatorTest.java
@@ -1,34 +1,30 @@
 package users.homework.codetotest;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class NewtonInterpolatorTest
 {
+    Interpolator interpolator;
+    List<Point> list;
+
+    @BeforeEach
+    public void setup()
+    {
+        interpolator = new NewtonInterpolator();
+        list = new ArrayList<>();
+    }
 
     @Test
-    public void getY_square()
+    public void getY_cubic_function()
     {
-        Interpolator interpolator = new NewtonInterpolator();
-        List<Point> list = new ArrayList<>();
-        for(float i = -5f; i <= 5f; i += 0.5f)
-        {
-            list.add(new Point(i, i * i));
-        }
-        interpolator.setPoints(list);
-        for(float i = -4.75f; i <= 4.75; i += 0.5f)
-        {
-            assertEquals(i * i, interpolator.getY(i), 0.0000001f);
-        }
-    }
-    @Test
-    public void getY_quadratic_function()
-    {
-        Interpolator interpolator = new NewtonInterpolator();
-        List<Point> list = new ArrayList<>();
         for(float i = -5f; i <= 5f; i += 0.5f)
         {
             list.add(new Point(i, i * i * i + 3f * i * i + 8f * i + 0.34f));
@@ -45,8 +41,6 @@ public class NewtonInterpolatorTest
     @Test
     public void getY_sine_function()
     {
-        Interpolator interpolator = new NewtonInterpolator();
-        List<Point> list = new ArrayList<>();
         for(double i = 0; i <= 360; i += 30)
         {
             list.add(new Point((float) Math.toRadians(i),
@@ -63,8 +57,6 @@ public class NewtonInterpolatorTest
     @Test
     public void getY_sine_quadratic_function()
     {
-        Interpolator interpolator = new NewtonInterpolator();
-        List<Point> list = new ArrayList<>();
         for(double i = 0; i <= 360; i += 30)
         {
             list.add(new Point((float) i, sine_quadratic_function(i)));
@@ -81,31 +73,50 @@ public class NewtonInterpolatorTest
     @Test
     public void getY_sine_4th_order_function()
     {
-        Interpolator interpolator = new NewtonInterpolator();
-        List<Point> list = new ArrayList<>();
         for(double i = 0; i <= 180; i += 15)
         {
-            list.add(new Point((float) i, break_it_for_sure(i)));
+            list.add(new Point((float) i, func_4th_order(i)));
         }
         interpolator.setPoints(list);
         for(double i = 5; i <= 180; i += 15)
         {
-            assertEquals(break_it_for_sure(i),
+            assertEquals(func_4th_order(i),
                     interpolator.getY((float) i),
                     0.01f);
         }
     }
 
+    @Test
+    public void getY_quadric_tan()
+    {
+        for(double i = 0; i <= 180; i += 3)
+        {
+            list.add(new Point((float) i, tan_func(i)));
+        }
+        interpolator.setPoints(list);
+        for(double i = 1.5; i <= 180; i += 3)
+        {
+            assertEquals(tan_func(i),
+                    interpolator.getY((float) i),
+                    0.1f);
+        }
+    }
 
     private float sine_quadratic_function(double x)
     {
         return (float) (Math.pow(Math.sin(Math.toRadians(x)), 2) + 3.48f * Math.cos(Math.toRadians(x)) + 4.31f);
     }
-    private float break_it_for_sure(double x)
+
+    private float func_4th_order(double x)
     {
         return (float) (8.431f * Math.pow(Math.cos(Math.toRadians(x)), 4)
                 + 3.483f * Math.pow(Math.sin(Math.toRadians(x)), 3)
                 + 9.388f * Math.pow(Math.sin(Math.toRadians(x)), 2)
                 + 1.333f * Math.cos(Math.toRadians(x)) + 23.1222f);
+    }
+
+    private float tan_func(double x)
+    {
+        return (float) (Math.pow(Math.tan(Math.toRadians(x * 0.5)), 2) - Math.tan(Math.toRadians(x)));
     }
 }
