4724b59db643847d005b4c24a70c23145ff7b5b9
Sergey Zhak <s.zhak@semrush.com>
2021-07-22 18:50:05 +0300

    Suppressed beta

--- src/test/java/users/homework/codetotest/UniversalTest.java
+++ src/test/java/users/homework/codetotest/UniversalTest.java
@@ -14,6 +14,7 @@ import java.util.stream.Stream;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
+@SuppressWarnings("UnstableApiUsage")
 public class UniversalTest<T extends Interpolator> {
 
     private final Supplier<? extends T> interpolator;
